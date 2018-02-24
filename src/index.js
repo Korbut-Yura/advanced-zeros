4764537b8584fb90b9a2f5efc70e7f4a1f940290
Yura-Korbut <korbuty@mail.ru>
2018-02-24 16:55:36 +0300

    Solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
-}
+        let result = 0,
+          i = 1;
+    
+        base = findBase(base);
+    
+        function findBase(base) {
+            let m = 1;
+            for (let j = 2; j <= base; j++) {
+                if (base % j == 0) {
+                    while (base % j == 0 && base > j) {
+                        base /= j;
+                        m++;
+                    }
+                    if (base != j) {
+                        m = 1
+                        findBase(base);
+                    }
+                    else {
+                        return [base,m];
+                    }
+                }
+            }
+        }
+
+        while (Math.pow(base[0],i) < number){
+            result += Math.floor(number/Math.pow(base[0],i));
+            i++;
+        }
+    
+        return Math.floor(result/base[1]);
+  }
